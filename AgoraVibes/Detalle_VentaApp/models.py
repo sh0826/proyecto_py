fad021c20878172ffafbd4ab1632c11f3dd71cb3
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-24 23:47:08 -0500

    add: subo
    subo modelo producto con migraciones, solo escribi el modelo detalle venta pero no la migre

--- AgoraVibes/Detalle_VentaApp/models.py
+++ AgoraVibes/Detalle_VentaApp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ProductoApp.models import Producto
+'''from VentaApp.models import Venta'''
+from django.core.exceptions import ValidationError
+
+class DetalleVenta(models.Model):
+    '''MEDIO_PAGO = [
+        ('tarjeta', 'Tarjeta'),
+        ('efectivo', 'Efectivo'),
+        ('transferencia', 'Transferencia'),
+    ] va en venta'''
+    '''venta = models.ForeignKey(Venta, on_delete=models.CASCADE)'''
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    precio_unit = models.IntegerField()
+    cant_prod = models.IntegerField()
+    '''medio_pago = models.CharField(max_length=20, choices=MEDIO_PAGO) va en venta'''
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'detalle venta'
+        verbose_name_plural = 'detalles de ventas'
+
+    def clean(self):
+        if self.cant_prod > self.producto.stock:
+            raise ValidationError("No hay suficiente cantidad del producto")
+        
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.producto.stock -= self.cant_prod
+            self.producto.save()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.producto}-{self.cant_prod}"
 
 # Create your models here.
--- AgoraVibes/ProductoApp/models.py
+++ AgoraVibes/ProductoApp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Producto (models.Model):
+    TIPO_PRODUCTO = [
+        ('licor', 'Licor'),
+        ('cerveza', 'Cerveza'),
+        ('cigarrillo', 'Cigarrillo'),
+    ]
+    UNIDAD_MD = [
+        ('ml', "Ml"),
+        ('l', "L"),
+        ('paquete', 'Paquete'),
+        ('medio', 'Medio'),
+    ]
+    nombre = models.CharField(max_length=20)
+    tipo = models.CharField(max_length=20, choices=TIPO_PRODUCTO)
+    cantidad_MD = models.IntegerField(null=True, blank=True)
+    unidad_MD = models.CharField(max_length=8, choices=UNIDAD_MD, null=True, blank=True) 
+    stock = models.IntegerField()
+    precio_unitario = models.IntegerField()
+    imagen = models.ImageField(upload_to="ProductoApp")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "producto"
+        verbose_name_plural = "productos"
+
+    def clean(self):
+        if self.unidad_MD and not self.cantidad_MD:
+            raise ValidationError("Debe ingresar cantidad si hay unidad")
+        if self.cantidad_MD and not self.unidad_MD:
+            raise ValidationError("Debe ingresar unidad si hay cantidad")
+        
+    def __str__(self):
+        if self.unidad_MD:
+            return f"{self.nombre} - {self.cantidad_MD}{self.unidad_MD}" 
+        else:
+            return self.nombre
+           
