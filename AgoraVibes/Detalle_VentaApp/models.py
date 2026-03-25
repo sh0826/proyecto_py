9ef585a1645bdb2baa6850c5d6a0cba64eebe38b
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-25 16:13:36 -0500

    add: noooo

--- AgoraVibes/Detalle_VentaApp/models.py
+++ AgoraVibes/Detalle_VentaApp/models.py
@@ -11,8 +11,8 @@ class DetalleVenta(models.Model):
         ('transferencia', 'Transferencia'),
     ] va en venta'''
     '''venta = models.ForeignKey(Venta, on_delete=models.CASCADE)'''
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    precio_unit = models.IntegerField()
     cant_prod = models.IntegerField()
     '''medio_pago = models.CharField(max_length=20, choices=MEDIO_PAGO) va en venta'''
     created = models.DateTimeField(auto_now_add=True)
@@ -34,5 +34,4 @@ class DetalleVenta(models.Model):
 
     def __str__(self):
         return f"{self.producto}-{self.cant_prod}"
-
 # Create your models here.
