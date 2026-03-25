64248cdffc16b9793a6a3b2332c104586bb47dd0
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-25 16:56:42 -0500

    add: inicio de apliacion

--- AgoraVibes/AgoraVibes/settings.py
+++ AgoraVibes/AgoraVibes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AgoraVibesApp',
     'ProductoApp',
     'Detalle_VentaApp',
     'EventoApp',
--- AgoraVibes/AgoraVibes/urls.py
+++ AgoraVibes/AgoraVibes/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from AgoraVibes import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('AgoraVibesApp.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
