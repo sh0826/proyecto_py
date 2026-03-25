344de50ab33d0711ee3c48b2834d16e925bd66a2
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-25 16:15:27 -0500

    comentado

--- AgoraVibes/Detalle_VentaApp/models.py
+++ AgoraVibes/Detalle_VentaApp/models.py
@@ -9,12 +9,12 @@ class DetalleVenta(models.Model):
         ('tarjeta', 'Tarjeta'),
         ('efectivo', 'Efectivo'),
         ('transferencia', 'Transferencia'),
-    ] va en venta'''
-    '''venta = models.ForeignKey(Venta, on_delete=models.CASCADE)'''
+    ] va en venta
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cant_prod = models.IntegerField()
-    '''medio_pago = models.CharField(max_length=20, choices=MEDIO_PAGO) va en venta'''
+    medio_pago = models.CharField(max_length=20, choices=MEDIO_PAGO) va en venta
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -35,3 +35,4 @@ class DetalleVenta(models.Model):
     def __str__(self):
         return f"{self.producto}-{self.cant_prod}"
 # Create your models here.
+'''
