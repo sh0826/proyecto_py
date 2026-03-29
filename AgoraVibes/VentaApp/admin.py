cc9a444c7826e51b4a2c0c1b8e0b22f7ac31adc9
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-03-29 02:52:20 -0500

    add

--- AgoraVibes/VentaApp/admin.py
+++ AgoraVibes/VentaApp/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Venta
+from django.urls import reverse
+from django.http import HttpResponseRedirect 
  #Register your models here.
-admin.site.register(Venta)
+
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'fecha', 'medio_pago')
+
+    def response_add(self, request, obj, post_url_continue = None):
+        url = reverse('admin:Detalle_VentaApp_detalleventa_add')
+        return HttpResponseRedirect(f'{url}?venta={obj.id}')
+admin.site.register(Venta, VentaAdmin)
