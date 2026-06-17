c40729d32844e505da323a53d999ea7715f52954
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-06-17 10:32:04 -0500

    add

--- AgoraVibes/AgoraVibes/settings.py
+++ AgoraVibes/AgoraVibes/settings.py
@@ -142,21 +142,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-]   
+]
 
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = 'AgoraVibes/media/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
