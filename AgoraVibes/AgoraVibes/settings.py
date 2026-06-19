d092a2fad957539c9c140b4741d8cfb2f0a6c0a4
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-06-18 21:27:01 -0500

    dios

--- AgoraVibes/AgoraVibes/settings.py
+++ AgoraVibes/AgoraVibes/settings.py
@@ -144,12 +144,14 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'static',
 ]
 
+# ¡AGREGA ESTA LÍNEA JUSTO AQUÍ ABAJO!
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
