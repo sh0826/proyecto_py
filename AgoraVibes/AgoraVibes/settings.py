da405274d2bf06fbcc76a333d47ab9da0106ebc6
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2026-06-19 02:16:55 -0500

    a ver

--- AgoraVibes/AgoraVibes/settings.py
+++ AgoraVibes/AgoraVibes/settings.py
@@ -14,7 +14,7 @@ import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
